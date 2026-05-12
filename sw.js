00add649e324578f55316576893e3c47847c0d2e
iwamotoco-source <iwamoto.co@ir-inc.net>
2026-05-12 17:29:26 +0900

    Add service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='pwa-v1';
+const ASSETS=['/calendar207777/','/calendar207777/index.html','/calendar207777/manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/calendar207777/index.html'))));});
